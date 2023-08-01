8f637a8dc1259feb86a2d599853ebc435926526a
milon-ahamed <milonahamed2468@gmail.com>
2023-08-01 14:14:09 +0600

    Second eddition

--- Generate_RGB_Color_by_Clicking/app.js
+++ Generate_RGB_Color_by_Clicking/app.js
@@ -1,12 +1,14 @@
 let button = document.querySelector('.text');
 let body = document.querySelector('body');
-// let colorName = document.querySelector('.colorname');
+let colorName = document.querySelector('.colorPic');
 
 
 
 button.addEventListener('click', ()=>{
   let color =  generateColor();
   body.style.backgroundColor = color;
+ colorName.textContent = color;
+ console.log(color);
 })
 
 function generateColor(){
